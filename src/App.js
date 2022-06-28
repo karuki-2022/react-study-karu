d365b4022f6a53815a548a5d71206b8784ef1b2b
karuki-2022 <107757433+karuki-2022@users.noreply.github.com>
2022-06-28 19:10:49 +0900

    React Routerまで

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useCallback, useState } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
+import { CssModules } from "./components/CssModules";
 
 export default function App() {
+  // const [text, setText] = useState(``);
+  // const [open, setOpen] = useState(false);
+
+  // const onChangeText = (e) => setText(e.target.value);
+
+  // const onClickOpen = () => setOpen(!open);
+
+  // const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        {/* <CssModules />
+        <input value={text} onChange={onChangeText} />
+        <br />
+        <br />
+        <button onClick={onClickOpen}>表示</button>
+        <ChildArea open={open} onClickClose={onClickClose} /> */}
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
